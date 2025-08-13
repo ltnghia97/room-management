5a6bdc9077eaad769933f8aebe815a5550372f2e
Lê Nghĩa <39977060+ltnghia97@users.noreply.github.com>
2025-08-13 14:40:12 +0700

    init

---
+++ rental-room/src/main/java/org/rootle/rentalroom/RentalRoomApplication.java
@@ -0,0 +1,13 @@
+package org.rootle.rentalroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalRoomApplication.class, args);
+    }
+
+}
---
+++ rental-room/src/test/java/org/rootle/rentalroom/RentalRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rootle.rentalroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
